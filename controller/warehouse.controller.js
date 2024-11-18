b83f01fa4050a25b36aebb3554f58be052b43653
unknown <mdamir4298@gmail.com>
2024-11-18 21:14:45 +0530

    warehouse routes for add delete and update

--- controller/warehouse.controller.js
+++ controller/warehouse.controller.js
@@ -13,5 +13,61 @@ const getWarehouses = async (req, res) => {
 };
 
 
+const addWarehouse = async (req, res) => {
+    try {
+        const { name, location } = req.body;
+        
+        const addwarehousedata = new warehouseModel({
+            name,
+            location
+        });
 
-module.exports={getWarehouses}
+        await addwarehousedata.save();
+        res.status(201).json({ message: "added successfully", warehouse: addwarehousedata });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+
+const updateWarehouse = async (req, res) => {
+    try {
+        const {id } = req.params;
+        const updateData = req.body;
+
+        const updatedWarehouse = await warehouseModel.findByIdAndUpdate(
+            id,
+            updateData,
+            { new: true } 
+        );
+
+        if (!updatedWarehouse) {
+            return res.status(404).json({ message: "not found" });
+        }
+
+        res.status(200).json({ message: "updated successfully", warehouse: updatedWarehouse });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+
+const deleteWarehouse = async (req, res) => {
+    try {
+        const { id } = req.params;
+        
+        const deleted = await warehouseModel.findByIdAndDelete(id);
+        
+        if (!deleted) {
+            return res.status(404).json({ message: "not found" });
+        }
+
+        res.status(200).json({ message: "deleted successfully" });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+
+module.exports={getWarehouses,addWarehouse,deleteWarehouse}
